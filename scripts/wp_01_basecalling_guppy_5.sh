374aff4804e841b018800ea617d76a952a0fb82b
SebastianVonRotz <43199441+SebastianVonRotz@users.noreply.github.com>
2020-10-26 17:13:41 +0100

    Further qcat scripts and basecalling test

--- scripts/wp_01_basecalling_guppy_5.sh
+++ scripts/wp_01_basecalling_guppy_5.sh
@@ -21,8 +21,8 @@ module load guppy/3.3.3
 
 export $(cat ./$1)
 
-mkdir -p $DATAPATH_BASECALLING_TEST_4_OUT
+mkdir -p $DATAPATH_BASECALLING_TEST_5_OUT
  
 # Run the guppy application here guppy_basecaller --help guppy_basecaller --print_workflows --compress_fastq
  
-guppy_basecaller -i $DATAPATH_BASECALLING_TEST_IN -s $DATAPATH_BASECALLING_TEST_4_OUT --cpu_threads_per_caller $SLURM_CPUS_PER_TASK --num_callers $SLURM_NTASKS_PER_NODE -c dna_r9.4.1_450bps_hac.cfg
+guppy_basecaller -i $DATAPATH_BASECALLING_TEST_IN -s $DATAPATH_BASECALLING_TEST_5_OUT --cpu_threads_per_caller $SLURM_CPUS_PER_TASK --num_callers $SLURM_NTASKS_PER_NODE -c dna_r9.4.1_450bps_hac.cfg
