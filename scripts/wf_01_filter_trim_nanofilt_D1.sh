bc7494baa9fb89855a84c5402ed49d53889a6937
von Rotz Sebastian <voro@zhaw.ch>
2020-11-11 13:31:22 +0100

    Test of workpackage 1 and filtering Dataset 1 and 2

---
+++ scripts/wf_01_filter_trim_nanofilt_D1.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+#SBATCH --job-name=guppy-batch_basecalling
+#
+#SBATCH --nodes=1 
+#SBATCH --ntasks-per-node=1 
+#SBATCH --cpus-per-task=32
+#
+#SBATCH --time=96:00:00 
+#SBATCH --partition=single 
+#SBATCH --qos=single
+#
+#SBATCH --mail-type=end 
+#SBATCH --mail-user=voro@zhaw.ch
+#
+ 
+# Load modules of dependencies module purge
+module load USS/2020
+module load gcc/7.3.0 
+module load python/3.6.5-pe5.26
+module load openblas/0.3.5-haswell-ep
+module load py-nanofilt/2.7.1-openblas-py3.6-pe5.26
+
+
+export $(cat ./$1)
+mkdir -p $DATAPATH_FILTER_TRIM_NANOFILT_D1_OUT
+
+for fq in $DATAPATH_FILTER_TRIM_NANOFILT_D1_IN*.fastq
+  do
+    cat $fq | NanoFilt -q 7 -l 1500 --maxlength 2500  > $DATAPATH_FILTER_TRIM_NANOFILT_D1_OUT$(basename $fq)
+  done
+
+
