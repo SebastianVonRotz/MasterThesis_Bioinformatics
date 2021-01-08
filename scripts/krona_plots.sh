7fc63157213ec990d75b12ebfc32f38b4b88b808
voro <voro@zhaw.ch>
2021-01-08 14:42:55 +0100

    New scripts for classification and reporting

--- scripts/krona_plots.sh
+++ scripts/krona_plots.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#SBATCH --job-name=kraken2_Classification
+#SBATCH --job-name=Krona_Plots
 #
 #SBATCH --nodes=1 
 #SBATCH --ntasks-per-node=1 
@@ -12,30 +12,27 @@
 #
 #SBATCH --mail-type=end 
 #SBATCH --mail-user=voro@zhaw.ch
-#SBATCH --output=qcat_output
 #
 
 # Export the env file 
 export $(cat env)
 
 # Define Output and Input Path
-DATA_PATH_IN=../results/$DATASET_NAME/Demultiplexing_qcat_$RUN_NAME_OUT/
-DATA_PATH_OUT=../results/$DATASET_NAME/Classification_Kraken2_$RUN_NAME_OUT/
+DATA_PATH_IN=../results/$DATASET_NAME/Classification_Kraken2_$RUN_NAME_OUT/
+DATA_PATH_OUT=../results/$DATASET_NAME/Report_Krona_$RUN_NAME_OUT/
 
 # Create Output dir
 mkdir -p $DATA_PATH_OUT
 
-# Run the Classification for each barcode fastq file
-for fq in $DATA_PATH_IN*.fastq
+# Run the creation of plots
+for fq in $DATA_PATH_IN*REPORT
   do
-    $PATH_KRAKEN2 --report $DATA_PATH_OUT$(basename $fq)_REPORT --db $PATH_KRAKEN2_DB $fq > $DATA_PATH_OUT$(basename $fq)_CLASSIFICATION
+    cut -f3,5 $fq > $DATA_PATH_OUT$(basename $fq)_KRONA_IN
+    # cut -f3,5 BC04.fastq_REPORT > krona_BC04.in
+
+    $PATH_KRONATOOLS $DATA_PATH_OUT$(basename $fq)_KRONA_IN -o $DATA_PATH_OUT$(basename $fq)_Krona.html -t 2 -m 1
+    # /cfs/earth/scratch/voro/local_apps/krona/KronaTools-2.7.1/bin/bin/ktImportTaxonomy  krona_BC04.in -o krona_BC04.html -t 2 -m 1
   done
 
-  extract the column
-
-	cut -f3,5 BC04.fastq_REPORT > krona_BC04.in
-
-create the html file
 
 
-	/cfs/earth/scratch/voro/local_apps/krona/KronaTools-2.7.1/bin/bin/ktImportTaxonomy  krona_BC04.in -o krona_BC04.html -t 2 -m 1
---
+++ scripts/nanoplot_sum_notFinished.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+#SBATCH --job-name=Nanoplot
+#
+#SBATCH --nodes=1 
+#SBATCH --ntasks-per-node=1 
+#SBATCH --cpus-per-task=32
+#
+#SBATCH --time=96:00:00 
+#SBATCH --partition=single 
+#SBATCH --qos=single
+#
+#SBATCH --mail-type=end 
+#SBATCH --mail-user=voro@zhaw.ch
+#
+ 
+# Load modules of dependencies module purge
+module load slurm
+module load USS/2020
+module load gcc/7.3.0
+module load openblas/0.3.5-haswell-ep
+module load py-nanoplot/1.32.0-openblas-py3.6-pe5.26
+
+# Export the env file 
+export $(cat env)
+
+# Define Output and Input Path
+DATA_PATH_IN=../results/$DATASET_NAME/Basecalling_Guppy_Array_$RUN_NAME_IN/
+DATA_PATH_OUT=../results/$DATASET_NAME/Report_nanoplot_$RUN_NAME_OUT/
+
+# Create Output dir
+mkdir -p $DATA_PATH_OUT
+
+# Coppy all fastq files to the nanoplot output folder
+cat $DATA_PATH_IN*.fastq > ${DATA_PATH_OUT}aggregated.fastq
+
+# Run the NanoPlot application
+NanoPlot --fastq  ${DATA_PATH_OUT}aggregated.fastq --loglength -o $DATA_PATH_OUT
+
+# Delete the aggregated fastq file
+rm ${DATA_PATH_OUT}aggregated.fastq
+
+					 
+
